95e130f578370d1b84e61cc8c9231cacf4cbd222
Daniel Dutra <93202813+DanielDutraMl@users.noreply.github.com>
2022-03-21 11:46:27 -0300

    Adicionando funcionalidades

--- index.js
+++ index.js
@@ -21,4 +21,41 @@ function diminuirValor()
 
 }
 
+function react() {
+	var reactNumero = document.getElementById("react")
+	if (reactNumero >= 1) {
+		return reactNumero
+	}else {
+		reactNumero.style.display = "none";
+}
+}
+
+function onlyOne(checkbox) {
+    var checkboxes = document.getElementsByName('check')
+    checkboxes.forEach((item) => {
+        if (item !== checkbox) item.checked = false
+    })
+}
+
+function myFunction() {
+	var checkBox = document.getElementById("cartao");
+	var text = document.getElementById("nomeCartao");
+	var text2 = document.getElementById("numeroCart")
+	var text3 = document.getElementById("nome")
+	var text4 = document.getElementById("nomePessoa")
+
+  
+	if (checkBox.checked == true){
+	  text.style.display = "block";
+	  text2.style.display = "block";
+	  text3.style.display = "block";
+	  text4.style.display = "block";
+	} else {
+	  text.style.display = "none";
+	  text2.style.display = "none"
+	  text3.style.display = "none"
+	  text4.style.display = "none"
+	}
+  }
+
   
